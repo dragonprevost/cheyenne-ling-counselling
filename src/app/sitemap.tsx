b49bca9c4cd4b63c98956835d6e6c4cd7851225e
Dragon Prevost <dragon@dkp.io>
2024-12-09 14:09:35 -0800

    Added sitemaps

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -8,5 +8,47 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 1,
     },
+    {
+      url: 'https://pembertonlandscaping.ca/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://pembertonlandscaping.ca/contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://pembertonlandscaping.ca/services/whistler',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://pembertonlandscaping.ca/services/pemberton',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://pembertonlandscaping.ca/services/maintenance',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://pembertonlandscaping.ca/services/patios',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://pembertonlandscaping.ca/services/hardscaping',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
   ]
 }
