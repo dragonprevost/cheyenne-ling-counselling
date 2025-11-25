82137fff0a62ba7ac5ab1b18ea0930b7c0520bc7
Cheyenne Ling <cheyenneling1@gmail.com>
2025-11-25 14:51:14 -0800

    updated site map

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -9,6 +9,36 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: "https://cheyennelingcounselling.com/blog/attachment_theory",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://cheyennelingcounselling.com/blog/understanding_emotions_in_relationships",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://cheyennelingcounselling.com/blog/effect_of_childhood_on_communication",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://cheyennelingcounselling.com/blog",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://cheyennelingcounselling.com/services",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
+    {
       url: "https://cheyennelingcounselling.com/about",
       lastModified: new Date(),
       changeFrequency: "monthly",
